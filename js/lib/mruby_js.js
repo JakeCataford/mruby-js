50e7859f5b6b4c471bf583021987dd711dbcdfd5
Jake Cataford <jakecataford@gmail.com>
2019-04-03 15:24:29 -0400

    dont call deprecated method

--- js/lib/mruby_js.js
+++ js/lib/mruby_js.js
@@ -7,7 +7,7 @@ mergeInto(LibraryManager.library, {
   __js_fetch_object: function (mrb, handle) {
     var obj = ___js_global_object()["MRB_JS_OBJ_CACHE"];
     if (obj && (typeof obj === 'object') &&
-        (typeof handle === 'number') && (handle > 0)) {
+      (typeof handle === 'number') && (handle > 0)) {
       return obj[handle];
     }
   },
@@ -16,7 +16,7 @@ mergeInto(LibraryManager.library, {
   __js_add_object: function (mrb, obj) {
     var global_object = ___js_global_object();
     global_object["MRB_JS_OBJ_CACHE"] = global_object["MRB_JS_OBJ_CACHE"] ||
-      {"_mruby_js_next_id": 1, "_mruby_js_recycled_ids": []};
+      { "_mruby_js_next_id": 1, "_mruby_js_recycled_ids": [] };
     var cache_object = global_object["MRB_JS_OBJ_CACHE"];
     var object_handle = -1;
 
@@ -35,9 +35,9 @@ mergeInto(LibraryManager.library, {
 
       if (Object.defineProperty) {
         Object.defineProperty(obj, "_mruby_js_id",
-                              {enumerable : false, value: new_id});
+          { enumerable: false, value: new_id });
         Object.defineProperty(obj, "_mruby_js_count",
-                              {enumerable : false, value: 1});
+          { enumerable: false, value: 1 });
       } else {
         obj["_mruby_js_id"] = new_id;
         obj["_mruby_js_count"] = 1;
@@ -63,11 +63,11 @@ mergeInto(LibraryManager.library, {
 
   __js_is_array: function (val) {
     return (typeof val !== 'undefined' &&
-            val && val.constructor === Array);
+      val && val.constructor === Array);
   },
 
   __js_fill_return_arg__deps: ['__js_add_object', '__js_is_floating_number',
-                               '__js_is_array'],
+    '__js_is_array'],
   __js_fill_return_arg: function (mrb, ret_p, val, parent_p) {
     var stack = 0;
     var RETURN_HANDLERS = {
@@ -122,7 +122,7 @@ mergeInto(LibraryManager.library, {
     // (at least in Chrome).
     // So we will use the old-fashioned way to do this:)
 
-    switch(args.length) {
+    switch (args.length) {
       case 0:
         return new func();
       case 1:
@@ -139,41 +139,41 @@ mergeInto(LibraryManager.library, {
         return new func(args[0], args[1], args[2], args[3], args[4], args[5]);
       case 7:
         return new func(args[0], args[1], args[2], args[3], args[4], args[5],
-                        args[6]);
+          args[6]);
       case 8:
         return new func(args[0], args[1], args[2], args[3], args[4], args[5],
-                        args[6], args[7]);
+          args[6], args[7]);
       case 9:
         return new func(args[0], args[1], args[2], args[3], args[4], args[5],
-                        args[6], args[7], args[8]);
+          args[6], args[7], args[8]);
       case 10:
         return new func(args[0], args[1], args[2], args[3], args[4], args[5],
-                        args[6], args[7], args[8], args[9]);
+          args[6], args[7], args[8], args[9]);
       default:
         assert(false, "We do not support that many arguments now-_-");
     }
   },
 
   __js_fetch_argument__deps: ['__js_fetch_object', '__js_fill_return_arg'],
-  __js_fetch_argument: function(mrb, argv_p, idx) {
+  __js_fetch_argument: function (mrb, argv_p, idx) {
     var TYPE_HANDLERS = {
-      0: function() { return false; }, // MRB_TT_FALSE
-      1: function() { return true; },  // MRB_TT_TRUE
+      0: function () { return false; }, // MRB_TT_FALSE
+      1: function () { return true; },  // MRB_TT_TRUE
       2: _mruby_js_get_integer,        // MRB_TT_FIXNUM
       3: _mruby_js_get_float,          // MRB_TT_FLOAT
-      4: function() {
+      4: function () {
         var handle = _mruby_js_get_object_handle.apply(null, arguments);
         return ___js_fetch_object(mrb, handle);
       },                        // MRB_TT_OBJECT
-      5: function() {
+      5: function () {
         var str_len = _mruby_js_get_string_len.apply(null, arguments);
         var str_p = _mruby_js_get_string_ptr.apply(null, arguments);
-        return Module['Pointer_stringify'](str_p, str_len);
+        return Module['UTF8ToString'](str_p, str_len);
       },                        // MRB_TT_STRING
-      6: function() { return undefined; }, // nil value
-      7: function() {
+      6: function () { return undefined; }, // nil value
+      7: function () {
         var proc = _mruby_js_get_proc.apply(null, arguments);
-        return function() {
+        return function () {
           var arena = _mrb_gc_arena_save(mrb);
           // Callback arguments
           var cargc = arguments.length;
@@ -185,8 +185,8 @@ mergeInto(LibraryManager.library, {
             cargv = _mruby_js_invoke_alloc_argv(mrb, cargc);
             for (i = 0; i < cargc; i++) {
               ___js_fill_return_arg(mrb,
-                                    _mruby_js_invoke_fetch_argp(mrb, cargv, i),
-                                    arguments[i], 0);
+                _mruby_js_invoke_fetch_argp(mrb, cargv, i),
+                arguments[i], 0);
             }
           }
           retp = _mruby_js_invoke_alloc_argv(mrb, 1);
@@ -205,15 +205,15 @@ mergeInto(LibraryManager.library, {
           return ret;
         };
       },                        // MRB_TT_PROC
-      8: function() {
+      8: function () {
         var handle = _mruby_js_get_array_handle.apply(null, arguments);
         return ___js_fetch_object(mrb, handle);
       },                        // MRB_TT_ARRAY
-      9: function() {
+      9: function () {
         var handle = _mruby_js_get_hash_handle.apply(null, arguments);
         return ___js_fetch_object(mrb, handle);
       },                        // MRB_TT_HASH
-      10: function() {
+      10: function () {
         _mruby_js_convert_symbol_to_string.apply(null, arguments);
         return TYPE_HANDLERS[5].apply(null, arguments);
       }                         // MRB_TT_SYMBOL
@@ -224,12 +224,12 @@ mergeInto(LibraryManager.library, {
   },
 
   js_invoke__deps: ['__js_fill_return_arg',
-                  '__js_fetch_object', '__js_invoke_using_new',
-                   '__js_fetch_argument', '__js_global_object'],
+    '__js_fetch_object', '__js_invoke_using_new',
+    '__js_fetch_argument', '__js_global_object'],
   js_invoke: function (mrb, this_value_p,
-                       func_handle,
-                       argv_p, argc,
-                       ret_p, type) {
+    func_handle,
+    argv_p, argc,
+    ret_p, type) {
     var func = ___js_fetch_object(mrb, func_handle);
     if (typeof func !== 'function') {
       _mruby_js_name_error(mrb);
@@ -260,7 +260,7 @@ mergeInto(LibraryManager.library, {
   },
 
   js_get_field__deps: ['__js_fill_return_arg', '__js_fetch_object',
-                       '__js_fetch_argument'],
+    '__js_fetch_argument'],
   js_get_field: function (mrb, obj_p, field_p, ret_p) {
     var handle = _mruby_js_get_object_handle(mrb, obj_p, 0);
     var obj = ___js_fetch_object(mrb, handle);
@@ -307,7 +307,7 @@ mergeInto(LibraryManager.library, {
           // Reset the next id when we have all recycled ids. I wonder
           // if a slice loop which can recycle partial ids is needed here.
           if (cache_object["_mruby_js_recycled_ids"].length ===
-              (cache_object["_mruby_js_next_id"] - 1)) {
+            (cache_object["_mruby_js_next_id"] - 1)) {
             cache_object["_mruby_js_next_id"] = 1;
             cache_object["_mruby_js_recycled_ids"] = [];
           }
@@ -317,7 +317,7 @@ mergeInto(LibraryManager.library, {
   },
 
   js_create_array__deps: ['__js_fetch_argument', '__js_fill_return_arg'],
-  js_create_array: function(mrb, arr_p, len, ret_p) {
+  js_create_array: function (mrb, arr_p, len, ret_p) {
     var ret = [], i;
     if ((arr_p !== 0) && (len !== -1)) {
       for (i = 0; i < len; i++) {
@@ -328,7 +328,7 @@ mergeInto(LibraryManager.library, {
   },
 
   js_create_empty_object__deps: ['__js_fill_return_arg'],
-  js_create_empty_object: function(mrb, ret_p) {
+  js_create_empty_object: function (mrb, ret_p) {
     ___js_fill_return_arg(mrb, ret_p, {}, 0);
   }
 });
